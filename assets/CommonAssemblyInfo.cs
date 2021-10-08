a2b3e9a0f32d62e47a0fa3b87d98ee2a4244ffa4
Richard Lawley <richard@richardlawley.com>
2021-10-08 18:58:53 +0100

    Update to netstandard, modernise projects

--- assets/CommonAssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using System.Reflection;
-
-[assembly: AssemblyVersion("4.8.2.2")]
-[assembly: AssemblyFileVersion("4.8.2.2")]
-[assembly: AssemblyInformationalVersion("4.8.2.2")]
-
-[assembly: CLSCompliant(true)]
